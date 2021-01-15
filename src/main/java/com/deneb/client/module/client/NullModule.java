63dcbd130aa691185cb4b1ba80cad9604c02e4e0
Noble hai lao <61795895+SpartanB312@users.noreply.github.com>
2021-01-15 12:24:35 +0000

    Delete src/main directory

--- src/main/java/com/deneb/client/module/client/NullModule.java
+++
@@ -1,8 +0,0 @@
-package com.deneb.client.module.client;
-
-import com.deneb.client.module.Category;
-import com.deneb.client.module.Module;
-
-@Module.Info(name = "NullModule",description = "Null Error!",category = Category.HIDDEN,visible = false)
-public class NullModule extends Module {
-}
